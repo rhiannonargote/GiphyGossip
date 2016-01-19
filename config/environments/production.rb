67ed981eafbc1a25499958da8010900f37e2967d
Rhiannon Argote <rhiannon.argote@gmail.com>
2016-01-20 10:42:21 +1100

    style changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,22 @@
 Rails.application.configure do
+  
+  # General Settings
+  config.app_domain = 'http://localhost:3000/'
+
+  # Email
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: config.app_domain }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com', 
+    port: '587',
+    enable_starttls_auto: true,
+    user_name: 'rhiannon',
+    password: 'foobar',
+    authentication: :plain,
+    domain: 'http://localhost:3000/'
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
