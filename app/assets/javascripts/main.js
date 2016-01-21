046d29a90e416b2d28aaa6908a23e509666e1473
Rhiannon Argote <rhiannon.argote@gmail.com>
2016-01-21 16:33:00 +1100

    removed undefined error from user show page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,16 +4,17 @@ var app = app || {}; // Create a global namespace (this is the way we group ever
 
 $(document).ready(function (){
   app.stories = new app.Stories();
-  app.stories.fetch();
+  app.stories.fetch().done(function () {
 
-// Create and interval that does a fetch every so often
-// // setInterval(function () {
-//   app.stories.fetch();
-// }, 1000); 
+  // Create and interval that does a fetch every so often
+  // // setInterval(function () {
+  //   app.stories.fetch();
+  // }, 1000); 
 
-// We create a new instance of the router  
-  app.router = new app.AppRouter();
+  // We create a new instance of the router  
+    app.router = new app.AppRouter();
 
-// This starts the router and makes it listen for changes  
-  Backbone.history.start();
+  // This starts the router and makes it listen for changes  
+    Backbone.history.start();
+  });
 });
