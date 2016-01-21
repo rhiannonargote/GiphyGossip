1a825d6e33c6816ed02fb42ef8ef6f416e71b28e
Kiah Hickson <kiah@kiahhickson.com.au>
2016-01-21 16:18:41 +1100

    resolved functionality for removing the your goss

--- app/assets/javascripts/views/StoryView.js
+++ app/assets/javascripts/views/StoryView.js
@@ -14,6 +14,7 @@ app.StoryView = Backbone.View.extend({
       app.stories.fetch();
     });
     this.$el.fadeOut();
+    $(".divViewShow, p#myGoss, div.bottomLine").fadeOut();
     // $(".divViewShowTitle").fadeOut();
   },
 
